283dd8d3eb2c7232c5bd60ddc7ad2159a95e2d64
Dar9586 <atoppi2012@gmail.com>
2023-06-03 19:21:42 +0200

    Refactoring Unite add-document-admin e add-document-admin Rimossa roba.html che non era usata Convertite var in let e const Ora tutti i campi sono convertiti in json se possibile e questo fixa la ricerca

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+import json
+from typing import Mapping, Any
+
 from bson import ObjectId
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
+
+# Number of items per page
+ITEMS_PER_PAGE = 25
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
 db = client["DBFilm"]
-movies_collection = db["movies_metadata"]
 
 
-@app.route('/create-document-admin', methods=['POST'])
 @app.route('/create-document', methods=['POST'])
 def create_object():
     field_names = request.form.getlist('field_name[]')
@@ -17,6 +21,12 @@ def create_object():
     # Create object dynamically
     new_object = dict()
     for name, value in zip(field_names, field_values):
+        if name == "_id":
+            value = ObjectId(value)
+        try:
+            value = json.loads(value)
+        except:
+            pass
         new_object[name] = value
 
     # Example: Accessing the dynamically created object
@@ -28,40 +38,44 @@ def create_object():
     return "Object created successfully!"
 
 
+def render_query(collection_name: str, query: Mapping[str, Any], page: int):
+    # Calculate the skip count based on the page number and items per page
+    skip = (page - 1) * ITEMS_PER_PAGE
+    collection = db[collection_name]
+    # Fetch the items from the collection with pagination
+    items = collection.find(query).skip(skip).limit(ITEMS_PER_PAGE)
+
+    # Count the total number of items in the collection
+    total_items = collection.count_documents(query)
+
+    # Calculate the total number of pages based on the total items and items per page
+    total_pages = total_items // ITEMS_PER_PAGE + (1 if total_items % ITEMS_PER_PAGE > 0 else 0)
+    return render_template('view-data.html', col_name=collection_name, items=items, total_page=total_pages,
+                           current_page=page)
+
+
 @app.route('/apply-search', methods=['GET'])
 def apply_search():
     page = int(request.args.get("page"))
     field_names = request.args.getlist('field_name[]')
     field_operation = request.args.getlist('field_operation[]')
     field_values = request.args.getlist('field_value[]')
-    print(field_names, field_operation, field_values, sep="\n")
     table = request.args.get("table")
     # Create object dynamically
     new_object = dict()
     for name, op, value in zip(field_names, field_operation, field_values):
+        if name == "_id":
+            value = ObjectId(value)
+        try:
+            value = json.loads(value)
+        except:
+            pass
         new_object[name] = {op: value}
-
-    # Example: Accessing the dynamically created object
-    print(field_names, field_operation, field_values, new_object, sep="\n")
-    cursor = db[table].find(new_object)
-    count = db[table].count_documents(new_object)
-    print(count)
-    skip = (page - 1) * ITEMS_PER_PAGE
-    # Fetch the items from the collection with pagination
-    items = cursor
-
-    # Count the total number of items in the collection
-    total_items = count
-
-    # Calculate the total number of pages based on the total items and items per page
-    total_pages = total_items // ITEMS_PER_PAGE + (1 if total_items % ITEMS_PER_PAGE > 0 else 0)
-    print(items)
-    return render_template('view-data.html', col_name=table, items=items, total_page=total_pages,
-                           current_page=page)
+    return render_query(table, new_object, page)
 
 
 @app.route('/get-fields')
-def ciao():
+def get_field_of_collection():
     selected_collection = request.args.get('collection')
     # Use aggregation to get all unique fields
     pipeline = [
@@ -98,28 +112,11 @@ def ciao():
     return jsonify(fields)
 
 
-# Number of items per page
-ITEMS_PER_PAGE = 10
 
 
 @app.route('/show/<collection_name>/<page>')
 def get_data(collection_name, page):
-    # Get the current page number from the query parameter
-    page = int(page)
-    # Calculate the skip count based on the page number and items per page
-    skip = (page - 1) * ITEMS_PER_PAGE
-    collection = db[collection_name]
-    # Fetch the items from the collection with pagination
-    items = collection.find().skip(skip).limit(ITEMS_PER_PAGE)
-
-    # Count the total number of items in the collection
-    total_items = collection.count_documents({})
-
-    # Calculate the total number of pages based on the total items and items per page
-    total_pages = total_items // ITEMS_PER_PAGE + (1 if total_items % ITEMS_PER_PAGE > 0 else 0)
-    print(items)
-    return render_template('view-data.html', col_name=collection_name, items=items, total_page=total_pages,
-                           current_page=page)
+    return redirect(url_for("apply_search", page=page, table=collection_name))
 
 
 @app.route('/delete/<collection_name>/<document_id>', methods=['POST'])
@@ -133,41 +130,23 @@ def delete_document(collection_name, document_id):
     return jsonify({'deleted_count': result.deleted_count})
 
 
-# Custom Jinja2 filter
-@app.template_filter('range_custom')
-def range_custom(start, end):
-    return range(start, end)
-
-
-@app.route('/add-document')
-def add_document():
-    # Get field names from the collection
-    field_names = list(movies_collection.find_one().keys())
-    field_names.remove("_id")
-    field_names.sort()
-    collection_names = db.list_collection_names()
-    collection_names.sort()
-    return render_template('add-document.html', col=collection_names)
-
-
 @app.route('/search')
 def search():
-    # Get field names from the collection
-    field_names = list(movies_collection.find_one().keys())
-    field_names.remove("_id")
-    field_names.sort()
     collection_names = db.list_collection_names()
     collection_names.sort()
     return render_template('search-document.html', col=collection_names)
 
 
-@app.route('/add-document-admin')
+@app.route('/add-document/admin')
 def add_document_admin():
-    # Get field names from the collection
-    field_names = list(movies_collection.find_one().keys())
-    field_names.remove("_id")
-    field_names.sort()
-    return render_template('add-document-admin.html')
+    return render_template('add-document.html', is_admin=True)
+
+
+@app.route('/add-document')
+def add_document():
+    collection_names = db.list_collection_names()
+    collection_names.sort()
+    return render_template('add-document.html', col=collection_names, is_admin=False)
 
 
 if __name__ == '__main__':
